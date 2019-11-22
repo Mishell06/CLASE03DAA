16f08132b0cc1c78b761d3f5e69b1a929185319a
Administrador <Administrador@DESKTOP-5E85M15>
2019-11-21 22:03:04 -0500

    1comit

---
+++ LoginDemoApplication/public_html/JS/loginJs.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#login").click(function(){
+        var user =$("#user").val();
+         var clave =$("#clave").val();
+         
+         if( user ==''|| clave ==''){
+             alert("por favor complete los campos ");
+         }
+    });
+});
